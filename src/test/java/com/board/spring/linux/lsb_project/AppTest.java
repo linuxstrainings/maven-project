a0a96405752ce6a9c81deb1e8843a289c1c462c1
linuxSpringBoard <linuxspringboard@gmail.com>
2023-08-12 16:05:00 -0400

    initial commit

---
+++ src/test/java/com/board/spring/linux/lsb_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.board.spring.linux.lsb_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
